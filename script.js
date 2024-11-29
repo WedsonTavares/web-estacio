59f3c14cafc11ca2908e74454ae80fd56f900de4
MarcosBezerra1 <bezerra_jr@icloud.com>
2024-11-29 20:12:43 -0300

    fix pag cadastro, html,php,js e css
    correção do bug do php

--- script.js
+++ script.js
@@ -15,16 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
       const addressResult = document.getElementById('addressResult');
   
       // Limpa qualquer mensagem anterior
-      addressResult.innerHTML = '';
+      // addressResult.innerHTML = '';
   
       // Verifica se o CEP é válido (deve ter 8 dígitos e ser numérico)
       if (cep.length !== 8 || isNaN(cep)) {
         addressResult.innerHTML = 'Por favor, insira um CEP válido.';
         return;
       }
+      
   
       // URL da API para buscar o endereço com base no CEP
       const url = `https://viacep.com.br/ws/${cep}/json/`;
+      console.log (cep)
   
       // Faz uma requisição à API para buscar o endereço
       fetch(url)
@@ -44,67 +46,26 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById('logradouro').value = data.logradouro;
             document.getElementById('bairro').value = data.bairro;
             document.getElementById('cidade').value = data.localidade;
-            document.getElementById('estado').value = data.uf;
-            addressResult.innerHTML = 'Endereço preenchido. Você pode editá-lo.';
+            document.getElementById('estado').value = data.uf; 
+            console.log(data);
+            
+           
           }
         })
-        .catch(error => {
-          // Exibe uma mensagem de erro se a requisição falhar
-          addressResult.innerHTML = 'Erro ao buscar o endereço: ' + error.message;
-        });
+       
     });
   
     // Adiciona um event listener ao formulário para lidar com o envio
     document.getElementById('myForm').addEventListener('submit', function(event) {
-      // Previne o comportamento padrão de envio do formulário
-      //event.preventDefault();
+    
       // Exibe uma mensagem de sucesso
       alert('Formulário enviado com sucesso!');
+      window.open('PaginaFilme.html');
+      
     });
   });
 
 
-// //               VALIDAÇÃO DO FORMULÁRIO DE CADASTRO
-
-// function validaForm() {
-//     const nome = document.getElementById("nome").value;
-//     const endereco = document.getElementById(
-//         "endereco"
-//     ).value;
-//     const email = document.getElementById("email").value;
-
-//     let erroNome = document.getElementById("erro-nome");
-//     let erroEndereco = document.getElementById(
-//         "erro-endereco"
-//     );
-//     let erroEmail = document.getElementById("erro-email");
-
-//     erroNome.textContent = "";
-//     erroEndereco.textContent = "";
-//     erroEmail.textContent = "";
-
-//     let estaValido = true;
-
-//     if (nome === "" || /\d/.test(nome)) {
-//         erroNome.textContent = 
-//             "Por favor, entre um nome apropriado.";
-//         estaValido = false;
-//     }
-
-//     if (endereco === "") {
-//         erroEndereco.textContent = 
-//             "Por favor, entre um endereço válido";
-//         estaValido = false;
-//     }
-
-//     if (email === "" || !email.includes("@")) {
-//         erroEmail.textContent = 
-//             "Por favor, entre um email válido";
-//         estaValido = false;
-//     }
-
-//     return estaValido;
-// }
 
 
 document.addEventListener("DOMContentLoaded", function () {
